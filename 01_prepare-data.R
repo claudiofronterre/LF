37da717a20fdf0f08e45eb37ae17b83535d157ba
Claudio Fronterrè <cla.f90@gmail.com>
2018-04-26 11:18:44 +0100

    added generation of prediction grids and covariates

--- 01_prepare-data.R
+++ 01_prepare-data.R
@@ -1,6 +1,6 @@
 # Load required packages and functions
 if (!require("pacman")) install.packages("pacman")
-pkgs = c("sp", "mapview", "tmap") # package names
+pkgs = c("sp", "mapview", "tmap", "raster") # package names
 pacman::p_load(pkgs, character.only = T)
 
 # Load and clean data --------------
@@ -23,6 +23,9 @@ coordinates(lf_sp) <- ~ Longitude + Latitude
 proj4string(lf_sp) <- CRS("+init=epsg:4326")
 lf_sp <- spTransform(lf_sp, CRS("+init=epsg:3857 +units=km"))
 
+# Save the clean file 
+#saveRDS(as.data.frame(lf_sp), file = "data/lf_clean.rds")
+
 # Visualisation -----------------
 
 # Interactive visualisation of data
@@ -32,14 +35,14 @@ mapView(lf_sp, zcol = c("PoT", "Prevalence", "Method_1"), legend = T, burst = T,
 # Static visualization of data
 africa <- rgdal::readOGR("data/Africa.shp")
 proj4string(africa) <- CRS("+init=epsg:4326") 
-africa <- spTransform(africa, proj4string(lf))
+africa <- spTransform(africa, proj4string(lf_sp))
 
 map <- tm_shape(africa) +
   tm_borders("black") +
   tm_fill("white") +
        tm_shape(lf_sp) +
   tm_symbols(col = "Method_1", title.col = "Survey data by\ndiagnostic method", 
-             size = .05, border.col = "black", palette = c("red", "blue")) +
+             size = .25, border.col = "black", palette = c("red", "blue")) +
   tm_compass(position = "left") +
   tm_layout(bg.color = "lightblue", design.mode = F, legend.bg.color = "white", legend.frame = "black") +
   tm_scale_bar() 
@@ -48,6 +51,8 @@ map
 # tmap_mode(mode = "view")
 # map
 # ttm()
+
+save_tmap(tm = map, filename = "figs/map.pdf", width = 10, height = 12)
 
 # Select points where both diagnostics where used ------------------------------------------------
 ict <- which(lf$Method_1 == "Serological")
@@ -62,3 +67,19 @@ lf_both <- lf_both[, c("Country", "Latitude", "Longitude", "SurveyYear", "Examin
 lf_both$Prevalence_ict <- lf_both$Positive_ict/lf_both$Examined
 lf_both$Prevalence_mf <- lf_both$Positive_mf/lf_both$Examined
 saveRDS(lf_both, file = "data/lf_both.rds")
+
+# Prepare prediction  grid
+map <- raster("data/Mfprev_filled.tif")
+map10km <- aggregate(map, fact = 10, filename = "data/map10km.tif", format = "GTiff", overwrite=TRUE)
+map5km <- disaggregate(map10km, fact = 2, filename = "data/map5km.tif", format = "GTiff", overwrite=TRUE)
+pred_coords <- rasterToPoints(map5km)[,c(1, 2)]/1000
+saveRDS(pred_coords, file = "data/pred_coords.rds")
+
+# Prepare covariates from raster (path is the location of all the rasters) 
+coords <- data.frame(x = lf$Longitude, y = lf$Latitude) # extraction points
+coordinates(coords) <- ~ x + y
+proj4string(coords) <- CRS("+init=epsg:4326")
+coords <- spTransform(coords, CRS("+init=epsg:3857"))
+source("R/extract_raster.R")
+covariates <- extract_raster(path = "~/Documents/Raster_Covariates/", coords = coords)
+saveRDS(covariates, file = "data/covariates.rds")
